c192da65a4eac883d07f5189167148f1fd878227
Rafael Hiroshi <rtagata@brq.com>
2016-07-06 17:01:44 -0300

    Livro capitulo 5, erro de versao, analisar

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var load = require('express-load');
-
 var app = express();
+var cookieParser = require('cookie-parser');
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+app.use(express.cookieParser('ntalk'));
+app.use(express.session());
+app.use(express.json());
+app.use(express.urlencoded());
 app.use(express.static(__dirname + '/public'));
 
 
--- controllers/home.js
+++ controllers/home.js
@@ -1,8 +1,24 @@
 module.exports = function(app) {
 	var HomeController = {
 		index: function(req, res){
-			console.log('GGGGGGG');
 			res.render('home/index');
+		},
+		login: function(req, res){
+			var email = req.body.usuario.email;
+			var nome = req.body.usuario.nome;
+			if(email && nome){
+				var usuario = req.body.usuario;
+				usuario['contatos'] = [];
+				req.session.usuario = usuario;
+				req.redirect('/contatos');
+			}else{
+				res.redirect('/');
+			}
+		},
+
+		logout: function(req, res){
+			req.session.destroy();
+			res.redirect('/');
 		}
 	};
 	return HomeController;
